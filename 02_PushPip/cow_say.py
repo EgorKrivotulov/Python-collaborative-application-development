bdf5420077544da1cee21604ce7f1dae4d3a033c
EgorKrivotulov <egkrivotuloff@gmail.com>
2023-03-04 21:27:05 +0300

    starting 02 hw

---
+++ 02_PushPip/cow_say.py
@@ -0,0 +1,13 @@
+import cowsay
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", action="store_true")
+parser.add_argument("-e")
+parser.add_argument("-f")
+parser.add_argument("-W", type=int)
+parser.add_argument("-T")
+parser.add_argument("message", action="store", default="", nargs="?")
+params = parser.parse_args()
+print(params)
+if (params.l == True and params.message == ""):
+    print(cowsat.list_cows)
--- cow_say.py
+++
@@ -1,2 +0,0 @@
-import cowsay
-help(cowsay)

0db557e5d50d0fcfbe4a489a031bf9faa26a0319
EgorKrivotulov <egkrivotuloff@gmail.com>
2023-03-04 21:56:26 +0300

    adding state parameter

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -7,7 +7,10 @@ parser.add_argument("-f")
 parser.add_argument("-W", type=int)
 parser.add_argument("-T")
 parser.add_argument("message", action="store", default="", nargs="?")
+parser.add_argument("state", choices=["-b", "-d", "-g", "-p", "-s", "-t", "-w", "-y"])
 params = parser.parse_args()
 print(params)
-if (params.l == True and params.message == ""):
+"""if (params.l == True and params.message == ""):
     print(cowsay.list_cows())
+else:
+    print(cowsay())"""

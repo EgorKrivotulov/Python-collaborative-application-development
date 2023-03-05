52d1cd1ba29cd5e8177e130521756881b9390f68
EgorKrivotulov <egkrivotuloff@gmail.com>
2023-03-05 20:07:05 +0300

    cowsay implemented (barely)

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -1,6 +1,6 @@
 import cmd
 import cowsay
-
+import shlex
 class mycmd(cmd.Cmd):
     intro = "How and what should We say?"
     prompt = "<>: "
@@ -8,5 +8,15 @@ class mycmd(cmd.Cmd):
     def do_exit(self, arg):
         'End command line'
         return 1
+    def do_cowsay(self, arg):
+        print(arg)
+        print(shlex.split(arg))
+        print(cowsay.cowsay(shlex.split(arg)[0]))
+    def do_list_cows(self, arg):
+        cowsay.list_cows()
+    def do_make_bubble(self, arg):
+        pass
+    def do_cowthink(self, arg):
+        pass
 if __name__ == "__main__":
     mycmd().cmdloop()

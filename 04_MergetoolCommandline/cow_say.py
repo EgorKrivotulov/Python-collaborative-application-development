676ccf6c63e508071c16906817455f9290992f48
EgorKrivotulov <egkrivotuloff@gmail.com>
2023-03-05 21:40:30 +0300

    cowsay, cowthink, make_bubble implemented (wider)

--- 04_MergetoolCommandline/cow_say.py
+++ 04_MergetoolCommandline/cow_say.py
@@ -9,14 +9,59 @@ class mycmd(cmd.Cmd):
         'End command line'
         return 1
     def do_cowsay(self, arg):
-        print(arg)
-        print(shlex.split(arg))
-        print(cowsay.cowsay(shlex.split(arg)[0]))
+        args = cow_parse(shlex.split(arg))
+        print(cowsay.cowsay(message=args['message'], eyes=args['-e'], tongue=args['-T'], cow=args['-f']))
     def do_list_cows(self, arg):
         print(cowsay.list_cows())
     def do_make_bubble(self, arg):
         print(cowsay.make_bubble(shlex.split(arg)[0]))
     def do_cowthink(self, arg):
-        print(cowsay.cowthink(shlex.split(arg)[0]))
+        args = cow_parse(shlex.split(arg))
+        print(cowsay.cowthink(message=args['message'], eyes=args['-e'], tongue=args['-T'], cow=args['-f']))
+
+def cow_parse(tokens):
+    args = {'-f':'default', 'message':'', '-e':'oo', '-T':'  '}
+    used_tokens = set()
+    for i in range(0, len(tokens)-1):
+        print(tokens[i], tokens[i] in args.keys())
+        if tokens[i] in args.keys() and tokens[i] != 'message':
+            args[tokens[i]] = tokens[i+1]
+            used_tokens.add(tokens[i])
+            used_tokens.add(tokens[i+1])
+
+    texts = list(set(tokens).difference(used_tokens))
+    if len(texts) == 0:
+        texts[0] = ''
+    if len (texts) != 1:
+        print("WARNING: Can't choose from that options for message:")
+        print(texts)
+    for i in tokens:
+        if i in texts:
+            args['message'] = i
+            break
+    return(args)
+def bubble_parse(tokens):
+    args = {'l':'<', 'r':'>'}
+    used_tokens = set()
+    for i in range(0, len(tokens)-1):
+        print(tokens[i], tokens[i] in args.keys())
+        if tokens[i] in args.keys() and tokens[i] != 'message':
+            args[tokens[i]] = tokens[i+1]
+            used_tokens.add(tokens[i])
+            used_tokens.add(tokens[i+1])
+
+    texts = list(set(tokens).difference(used_tokens))
+    if len(texts) == 0:
+        texts[0] = ''
+    if len (texts) != 1:
+        print("WARNING: Can't choose from that options for message:")
+        print(texts)
+    for i in tokens:
+        if i in texts:
+            args['message'] = i
+            break
+    return(args)
+            
+
 if __name__ == "__main__":
     mycmd().cmdloop()
